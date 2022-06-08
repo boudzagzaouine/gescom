20fe706522e503f92d822a7f355ee15d5f10d1da
abdennour bouaicha <bouaicha.abdennour@gmail.com>
2022-06-08 16:18:02 +0100

    .

--- src/features/manager/client/FormArticleClient.tsx
+++ src/features/manager/client/FormArticleClient.tsx
@@ -1,174 +1,156 @@
-import React, {
-  ChangeEvent,
-  forwardRef,
-  Ref,
-  useEffect,
-  useState,
-} from "react";
-import {
-  Article,
-  article0,
-  ArticleClient,
-  Client,
-  f0,
-  Fournisseur,
-} from "tools/types";
-import Modal from "widgets/Modal";
+import { OpenArticleProp, openArticles } from 'config/rtk/rtkArticle';
+import { OpenFournisseurProp, openFournisseurs } from 'config/rtk/rtkFournisseur';
+import dateFormat from 'dateformat';
+import React, { ChangeEvent, forwardRef, Ref, useEffect, useState } from 'react';
+import DatePicker from 'react-datepicker';
+import { Article, article0, ArticleClient, f0, Fournisseur } from 'tools/types';
+import { Field, Form } from 'widgets';
+import Bcancel from 'widgets/Bcancel';
+import Bsave from 'widgets/Bsave';
+import Calendar from 'widgets/Calendar';
+import Modal from 'widgets/Modal';
+
 //@ts-ignore
-import dateFormat from "dateformat";
-import DatePicker from "react-datepicker";
-import Calendar from "widgets/Calendar";
-import { Field, Form } from "widgets";
-import Bsave from "widgets/Bsave";
-import Bcancel from "widgets/Bcancel";
-import {
-  OpenFournisseurProp,
-  openFournisseurs,
-} from "config/rtk/rtkFournisseur";
-import { openArticles, OpenArticleProp } from "config/rtk/rtkArticle";
 type FormArticleClientProp = {
-  articleclient: ArticleClient;
-  // client:Client
-  refetchList: () => void;
-  add: () => void;
-  edit: () => void;
+	articleclient: ArticleClient;
+	// client:Client
+	refetchList: () => void;
+	add: () => void;
+	edit: () => void;
 };
 const FormArticleClient = (
-  { articleclient, refetchList, add, edit }: FormArticleClientProp,
-  ref: Ref<void>
+	{ articleclient, refetchList, add, edit }: FormArticleClientProp,
+	ref: Ref<void>,
 ) => {
-  const [showModal, setShowModal] = useState(false);
-  const [articleclient0, setArticleclient0] = useState(articleclient);
-  const [startDate, setStartDate] = useState(articleclient0.date);
-  const [openCalendar, setOpenCalendar] = useState(false);
-  const openModal = (a: ArticleClient) => {
-    setArticleclient0(a);
-    setShowModal(true);
-  };
-  const fournisseursOpen: OpenFournisseurProp = openFournisseurs();
-  const fournisseurs: Fournisseur[] = fournisseursOpen.data.content;
-  const famillArticlesOpen: OpenArticleProp = openArticles();
-  const famillArticles: Article[] = famillArticlesOpen.data.content;
-  const save = articleclient0.id == "" ? add : edit;
-  const close = () => {
-    setShowModal(false);
-  };
-  useEffect(() => {
-    //@ts-ignore
-    ref.current = openModal;
-  });
-  return (
-    <Modal
-      title={
-        articleclient0.id === ""
-          ? "Nouvel article client"
-          : "Mise à jour de l'article client"
-      }
-      show={showModal}
-      format={5}
-      close={close}
-    >
-      <Form defaultValues={articleclient0} onSubmit={save}>
-        <>
-          <div className="float-left w-1/2 relative">
-            <Field
-              label="design"
-              name="design"
-              onChange={(e: ChangeEvent<HTMLInputElement>) => {
-                setArticleclient0({
-                  ...articleclient0,
-                  design: e.target.value,
-                });
-              }}
-            />
-            <Field
-              label="Prix"
-              name="prix"
-              onChange={(e: ChangeEvent<HTMLInputElement>) => {
-                setArticleclient0({ ...articleclient0, prix: +e.target.value });
-              }}
-            />
-            <Field
-              label="Poid"
-              name="poid"
-              onChange={(e: ChangeEvent<HTMLInputElement>) => {
-                setArticleclient0({ ...articleclient0, poid: +e.target.value });
-              }}
-            />
-          </div>
-          <div className="float-left w-1/2">
-            <Field
-              label="Date"
-              name="date33"
-              value={dateFormat(startDate, "dd-mm-yyyy")}
-              onFocus={() => {
-                setOpenCalendar(true);
-              }}
-            />
-            {openCalendar && (
-              <DatePicker
-                selected={startDate}
-                name="date11"
-                onChange={(d: Date) => {
-                  setStartDate(d);
-                  setArticleclient0({ ...articleclient0, date: d });
-                  setOpenCalendar(false);
-                }}
-                dateFormat="dd-MM-yyyy"
-                calendarContainer={Calendar}
-                inline
-              />
-            )}
-            <Field
-              label="Fournisseur"
-              name="idFournisseur"
-              as="select"
-              onChange={(e: ChangeEvent<HTMLSelectElement>) => {
-                setArticleclient0({
-                  ...articleclient0,
-                  idFournisseur: e.target.value,
-                });
-              }}
-            >
-              {[f0, ...(fournisseurs || [])]?.map((c: Fournisseur) => (
-                <option value={c.id}>{c.design}</option>
-              ))}
-            </Field>
-            <Field
-              label="Famille Article"
-              name="idFamilleArticle"
-              as="select"
-              onChange={(e: ChangeEvent<HTMLSelectElement>) => {
-                setArticleclient0({
-                  ...articleclient0,
-                  idFamilleArticle: e.target.value,
-                });
-              }}
-            >
-              {[article0, ...(famillArticles || [])]?.map((c: Article) => (
-                <option value={c.id}>{c.design}</option>
-              ))}
-            </Field>
-          </div>
-          <Bsave
-            className="float-right mt-5 b-ajust-r"
-            onClick={() => {
-              setTimeout(() => {
-                refetchList();
-                close();
-              }, 600);
-            }}
-          />
-        </>
-      </Form>
-      <Bcancel
-        className="float-right mt-5 b-ajust"
-        onClick={() => {
-          close();
-        }}
-      />
-    </Modal>
-  );
+	const [showModal, setShowModal] = useState(false);
+	const [articleclient0, setArticleclient0] = useState(articleclient);
+	const [startDate, setStartDate] = useState(articleclient0.date);
+	const [openCalendar, setOpenCalendar] = useState(false);
+	const openModal = (a: ArticleClient) => {
+		setArticleclient0(a);
+		setShowModal(true);
+	};
+	const fournisseursOpen: OpenFournisseurProp = openFournisseurs();
+	const fournisseurs: Fournisseur[] = fournisseursOpen.data.content;
+	const famillArticlesOpen: OpenArticleProp = openArticles();
+	const famillArticles: Article[] = famillArticlesOpen.data.content;
+	const save = articleclient0.id == "" ? add : edit;
+	const close = () => {
+		setShowModal(false);
+	};
+	useEffect(() => {
+		//@ts-ignore
+		ref.current = openModal;
+	});
+	return (
+		<Modal
+			title={
+				articleclient0.id === ""
+					? "Nouvel article client"
+					: "Mise à jour de l'article client"
+			}
+			show={showModal}
+			format={5}
+			close={close}>
+			<Form defaultValues={articleclient0} onSubmit={save}>
+				<>
+					<div className='float-left w-1/2 relative'>
+						<Field
+							label='design'
+							name='design'
+							onChange={(e: ChangeEvent<HTMLInputElement>) => {
+								setArticleclient0({
+									...articleclient0,
+									design: e.target.value,
+								});
+							}}
+						/>
+						<Field
+							label='Prix'
+							name='prix'
+							onChange={(e: ChangeEvent<HTMLInputElement>) => {
+								setArticleclient0({ ...articleclient0, prix: +e.target.value });
+							}}
+						/>
+						<Field
+							label='Poid'
+							name='poid'
+							onChange={(e: ChangeEvent<HTMLInputElement>) => {
+								setArticleclient0({ ...articleclient0, poid: +e.target.value });
+							}}
+						/>
+					</div>
+					<div className='float-left w-1/2'>
+						<Field
+							label='Date'
+							name='date33'
+							value={dateFormat(startDate, "dd-mm-yyyy")}
+							onFocus={() => {
+								setOpenCalendar(true);
+							}}
+						/>
+						{openCalendar && (
+							<DatePicker
+								selected={startDate}
+								name='date11'
+								onChange={(d: Date) => {
+									setStartDate(d);
+									setArticleclient0({ ...articleclient0, date: d });
+									setOpenCalendar(false);
+								}}
+								dateFormat='dd-MM-yyyy'
+								calendarContainer={Calendar}
+								inline
+							/>
+						)}
+						<Field
+							label='Fournisseur'
+							name='idFournisseur'
+							as='select'
+							onChange={(e: ChangeEvent<HTMLSelectElement>) => {
+								setArticleclient0({
+									...articleclient0,
+									idFournisseur: e.target.value,
+								});
+							}}>
+							{[f0, ...(fournisseurs || [])]?.map((c: Fournisseur) => (
+								<option value={c.id}>{c.design}</option>
+							))}
+						</Field>
+						<Field
+							label='Famille Article'
+							name='idFamilleArticle'
+							as='select'
+							onChange={(e: ChangeEvent<HTMLSelectElement>) => {
+								setArticleclient0({
+									...articleclient0,
+									idFamilleArticle: e.target.value,
+								});
+							}}>
+							{[article0, ...(famillArticles || [])]?.map((c: Article) => (
+								<option value={c.id}>{c.design}</option>
+							))}
+						</Field>
+					</div>
+					<Bsave
+						className='float-right mt-5 b-ajust-r'
+						onClick={() => {
+							setTimeout(() => {
+								refetchList();
+								close();
+							}, 600);
+						}}
+					/>
+				</>
+			</Form>
+			<Bcancel
+				className='float-right mt-5 b-ajust'
+				onClick={() => {
+					close();
+				}}
+			/>
+		</Modal>
+	);
 };
 
 export default forwardRef(FormArticleClient);
--- src/features/manager/client/FormArticleCommande.tsx
+++ src/features/manager/client/FormArticleCommande.tsx
@@ -1,96 +1,86 @@
-import React, { ChangeEvent, useState } from "react";
-import { Field, Form } from "widgets";
-import Bcyan from "widgets/Bcyan";
-import {
-  arc0,
-  Commande,
-  ArticleCommande,
-  ArticleClient,
-  articleClient0,
-} from "tools/types";
-import Table from "widgets/Table";
-import Bcancel from "widgets/Bcancel";
-import Bsave from "widgets/Bsave";
-import {
-  OpenArticleClientByClientProp,
-  openArticleClientsByClient,
-} from "config/rtk/RtkArticleClient";
+import { OpenArticleClientByClientProp, openArticleClientsByClient } from 'config/rtk/RtkArticleClient';
+import React, { ChangeEvent, useState } from 'react';
+import { ArticleClient, articleClient0, ArticleCommande } from 'tools/types';
+import { Field, Form } from 'widgets';
+import Bcancel from 'widgets/Bcancel';
+import Bsave from 'widgets/Bsave';
+import Table from 'widgets/Table';
+
 type FormArticleCommandeProp = {
-  articleCommande: ArticleCommande;
-  idClient: string;
-  saveArticle: (art: ArticleCommande) => void;
-  close: () => void;
-  refetch: () => void;
-};
+	articleCommande: ArticleCommande;
+	idClient: string;
+	saveArticle: (art: ArticleCommande) => void;
+	close: () => void;
+	refetch: () => void;
+}; //
 const FormArticleCommande = ({
-  articleCommande,
-  idClient,
-  saveArticle,
-  close,
-  refetch,
+	articleCommande,
+	idClient,
+	saveArticle,
+	close,
+	refetch,
 }: FormArticleCommandeProp) => {
-  const articleClientsOpen: OpenArticleClientByClientProp =
-    openArticleClientsByClient(idClient);
-  const articlesClients: ArticleClient[] = articleClientsOpen.data;
-  const [articleCommande1, setArticleCommande1] =
-    useState<ArticleCommande>(articleCommande);
-  return (
-    <>
-      <tr className="relative">
-        <div className="absolute left-0 top-0 bg-[#ccc]">
-          <Form defaultValues={articleCommande1} onSubmit={saveArticle}>
-            <Table.td>
-              <Field
-                label=""
-                name="design"
-                as="select"
-                onChange={(e: ChangeEvent<HTMLSelectElement>) => {
-                  setArticleCommande1({
-                    ...articleCommande1,
-                    design: e.target.value,
-                  });
-                }}
-              >
-                {[articleClient0, ...(articlesClients || [])]?.map(
-                  (c: ArticleClient) => (
-                    <option value={c.design}>{c.design}</option>
-                  )
-                )}
-              </Field>
-            </Table.td>
-            <Table.td>
-              <Field name="qte" placeholder="qte" />
-            </Table.td>
-            <Table.td>
-              <Field name="portion" placeholder="portion" />
-            </Table.td>
-            <Table.td>
-              <Field name="pu" placeholder="pu" />
-            </Table.td>
-            <Table.td>
-              <div className="float-right w-full">
-                <Bsave
-                  onClick={() => {
-                    setTimeout(() => {
-                      refetch();
-                    }, 500);
-                  }}
-                />
-              </div>
-            </Table.td>
-          </Form>
-          <div className="float-right w-full">
-            <Bcancel
-              className="absolute right-0"
-              onClick={() => {
-                close();
-              }}
-            />
-          </div>
-        </div>
-      </tr>
-    </>
-  );
+	const articleClientsOpen: OpenArticleClientByClientProp =
+		openArticleClientsByClient(idClient);
+	const articlesClients: ArticleClient[] = articleClientsOpen.data;
+	const [articleCommande1, setArticleCommande1] =
+		useState<ArticleCommande>(articleCommande);
+	return (
+		<>
+			<tr className='relative'>
+				<div className='absolute left-0 top-0 bg-[#ccc]'>
+					<Form defaultValues={articleCommande1} onSubmit={saveArticle}>
+						<Table.td>
+							<Field
+								label=''
+								name='design'
+								as='select'
+								onChange={(e: ChangeEvent<HTMLSelectElement>) => {
+									setArticleCommande1({
+										...articleCommande1,
+										design: e.target.value,
+									});
+								}}>
+								{[articleClient0, ...(articlesClients || [])]?.map(
+									(c: ArticleClient) => (
+										<option value={c.design}>{c.design}</option>
+									),
+								)}
+							</Field>
+						</Table.td>
+						<Table.td>
+							<Field name='qte' placeholder='qte' />
+						</Table.td>
+						<Table.td>
+							<Field name='portion' placeholder='portion' />
+						</Table.td>
+						<Table.td>
+							<Field name='pu' placeholder='pu' />
+						</Table.td>
+						<Table.td>
+							<div className='float-right w-full'>
+								<Bsave
+									onClick={() => {
+										setTimeout(() => {
+											refetch();
+										}, 500);
+									}}
+								/>
+							</div>
+						</Table.td>
+					</Form>
+					<div className='float-right w-full'>
+						<Bcancel
+							className='absolute right-0'
+							onClick={() => {
+								close();
+							}}
+						/>
+					</div>
+				</div>
+			</tr>
+		</>
+	);
 };
 
 export default FormArticleCommande;
