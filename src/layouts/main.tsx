9e9f380835047092e707b3d278cc8f0275fc3368
abdennour bouaicha <bouaicha.abdennour@gmail.com>
2022-06-09 08:56:26 +0100

    repos dans v-abd est ce concentrer sur la version generique dans la branche v-generic

--- src/layouts/main.tsx
+++ src/layouts/main.tsx
@@ -50,7 +50,7 @@ const Layout = ({ children }: LayoutProps) => {
 		//if(loading && !session)window.location.href="/"
 		//
 	}, []);
-	const isBeta = true;
+	const isBeta = false;
 	const block = () => {
 		return (
 			<section className='bg-slate-100 float-left w-full'>
